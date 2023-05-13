07940878ed78ef87d5304043a41f35eba804cf8e
maxp421 <penzes.max@gmail.com>
2023-05-13 13:44:37 +0200

    Changed nvim-cmp autocomplete priority so that emmet_ls snippet suggestions would appear after lsp suggestions.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,6 +8,7 @@ lsp.ensure_installed({
   'lua_ls',
   'eslint',
   'html',
+  'emmet_ls',
 })
 
 -- Fix Undefined global 'vim'
@@ -41,6 +42,43 @@ lsp.set_preferences({
         info = 'I'
     }
 })
+
+-- make emmet_ls lower priority so lsp firt suggests autocomplete, then emmet snippets
+-- https://www.reddit.com/r/neovim/comments/woih9n/how_to_set_lsp_autocomplete_priority/
+-- I had the exact same issue. Changes the order of the sources didn't really work for me since the emmet snippets are coming from the nvim_lsp source not for 
+-- a snippet source and I still want lsp autocompletes to be of a high priority. I just want to de-prioritize emmet stuff.
+-- What I did was to write a custom comparison function and pass it to cmp's sorting.comparators setting. Like so 
+
+local types = require("cmp.types")
+
+local function deprioritize_snippet(entry1, entry2)
+  if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then return false end
+  if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then return true end
+end
+
+cmp.setup({
+  -- your config
+  sorting = {
+    priority_weight = 2,
+    comparators = {
+      deprioritize_snippet,
+      -- the rest of the comparators are pretty much the defaults
+      cmp.config.compare.offset,
+      cmp.config.compare.exact,
+      cmp.config.compare.scopes,
+      cmp.config.compare.score,
+      cmp.config.compare.recently_used,
+      cmp.config.compare.locality,
+      cmp.config.compare.kind,
+      cmp.config.compare.sort_text,
+      cmp.config.compare.length,
+      cmp.config.compare.order,
+    },
+  },
+})
+
+--This will put lsp snippets at the very bottom, so it's out of the way but still pretty easy to access.
+--There's probably a more elegant solution, but it's good enough for me. 
 
 lsp.setup()
 
